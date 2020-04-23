cd4a7400439d3207565d1b6c12b714634b22d9cc
Stephanie Raymos <stephanieraymos@gmail.com>
2020-04-23 15:31:29 -0700

    Added lots of react code and some folders with components + arrays

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
-/*import React, { Component } from 'react';
+ 
+import React, { Component } from 'react';
+import Directory from './components/Directory';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import { AUTHORS } from './shared/campsites';
 
 class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          authors: AUTHORS
+      };
+  }
+
     render() {
         return (
             <div className="App">
@@ -11,10 +21,10 @@ class App extends Component {
                     <NavbarBrand href="/">Stress Less</NavbarBrand>
                 </div>
                 </Navbar>
+                <Directory authors={this.state.authors} />
             </div>
         );
     }
 }
 
 export default App;
-
